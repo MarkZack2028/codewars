9438d9e082b71016e0b20abea1a6c54e42e4546f
Niko Aditya Alansyah <nikoalansyah24@gmail.com>
2024-10-06 21:33:26 +0700

    first commit

---
+++ src/jav/Jav.java
@@ -0,0 +1,18 @@
+package jav;
+//import java.util.Arrays;
+class Jav{
+    public static void main(String[]args){
+       //String vowels [] = {"a","i","u","e","o"};
+       String kata = "okeyy";
+       int count = 0;
+       for(int x = 0 ;x < kata.length() ; x++){
+           char s = kata.charAt(x);
+           if(s == 'a' || s == 'i' || s == 'u' || s == 'e' || s == 'o'){
+               count++;
+               //pakai increment untuk menghitung yang benar
+           }
+       }
+        System.out.println(count);
+   }
+}
+        
---
+++ src/jav/Javax.java
@@ -0,0 +1,14 @@
+package jav;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.Scanner;
+class Javax{
+    public static void main(String[]args){
+  int angka [] = {1,7,9,3};
+  Arrays.sort(angka);
+  int sum = angka[0] + angka[1];
+        System.out.println(sum);
+    }
+}
+        
